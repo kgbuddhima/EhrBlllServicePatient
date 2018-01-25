d8e268b0208edd775f8b47d03b9383383f854017
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2018-01-25 19:42:57 +0530

    SAL : Service URL strings list added

---
+++ EhrPatientBLLService/SAL/SvcURLs/SvcUrls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Utility;
+
+namespace SAL.SvcURLs
+{
+    public static class SvcUrls
+    {
+        public static string urlDeletePatient = UtilityLibrary.GetAppSettingValue("urlDeletePatient");
+        public static string urlGetPatientByPIN = UtilityLibrary.GetAppSettingValue("urlGetPatient");
+        public static string urlGetPatientByID = UtilityLibrary.GetAppSettingValue("urlGetPatient");
+        public static string urlGetNextPatientID = UtilityLibrary.GetAppSettingValue("urlGetNextPatientID");
+        public static string urlGetPatientCollection = UtilityLibrary.GetAppSettingValue("urlGetPatientCollection");
+        public static string urlInsertPatient = UtilityLibrary.GetAppSettingValue("urlInsertPatient");
+        public static string urlUpdatePatient = UtilityLibrary.GetAppSettingValue("urlUpdatePatient");
+    }
+}
