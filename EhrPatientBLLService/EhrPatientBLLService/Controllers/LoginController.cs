9e6ce52257ca1f889698b86103cd3c6204faee76
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2018-01-27 23:12:06 +0530

    PL : LoginController added

---
+++ EhrPatientBLLService/EhrPatientBLLService/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessEntity;
+using Utility;
+using SAL;
+
+namespace EhrPatientBLLService.Controllers
+{
+    [RoutePrefix("api/Login")]
+    public class LoginController : ApiController
+    {
+        SAL.IServiceProvider _document = new ServiceProvider();
+
+        [Route("ChekPatientLogin")]
+        [HttpPost]
+        public HttpResponseMessage CheckPatientLogin(Credentials credentials)
+        {
+            try
+            {
+                int patientId = _document.CheckPatientLogin(credentials);
+                if (patientId > 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, patientId);
+                }
+                else
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, CommonUnit.oFailed);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [Route("CheckStaffLogin")]
+        [HttpPost]
+        public HttpResponseMessage CheckStaffLogin(Credentials credentials)
+        {
+            try
+            {
+                int staffId = _document.CheckStaffLogin(credentials);
+                if (staffId > 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, staffId);
+                }
+                else
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, CommonUnit.oFailed);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
